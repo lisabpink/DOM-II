4d434bc6f526eec879471e1622d0ec59b1815f94
LISA_COOK <lisabpink@gmail.com>
2019-12-04 11:26:21 -0500

    10 of 10

--- js/index.js
+++ js/index.js
@@ -25,3 +25,77 @@ imgScale.style.transition = 'transform 0.5s';
 });
 
 
+//#4 changing background color
+
+const backgroundColor = document.querySelector('body');
+backgroundColor.addEventListener('click', (e) => {
+  backgroundColor.style.background ="aliceblue";
+   console.log(`event bubbled up again`);
+ })
+
+ //#5 doule clicking turns it back to white 
+
+ backgroundColor.addEventListener('dblclick', (e) => {
+   backgroundColor.style.background ="White";
+    console.log(`event bubbled up again`);
+  })
+
+  //#6 right clicking nav gives you a note
+
+  window.addEventListener("contextmenu", function(x) {
+    alert("Right clicking the nav gives you this pop-up!");
+  });
+
+
+  //#7 screen load pop up
+
+  window.addEventListener("load", function() {
+    window.prompt("Welcome to F U N  B U S !! Where would you like to go?");
+  });
+
+  //#8 zoom text in Lets Go!
+
+  function zoom(event) {
+    event.preventDefault();
+  
+    scale += event.deltaY * -0.01;
+  
+    scale = Math.min(Math.max(.125, scale), 4);
+  
+    el.style.transform = `scale(${scale})`;
+  }
+
+  let scale = 1;
+  const el = document.querySelector('.text-content');
+  el.onwheel = zoom;
+
+//#9 allows nav to scroll when you scroll
+
+const body = document.querySelector("body");
+	const nav = document.querySelector(".main-navigation");
+	body.addEventListener("keydown", function() {
+	  nav.style.position = "absolute";
+    });
+    
+    //#10
+
+    body.addEventListener("keypress", function() {
+        nav.style.position = "fixed";
+      });
+
+
+  //preventdefault and stoprop
+	const preventA = document.querySelectorAll("a");
+	
+	preventA.forEach(function(a) {
+	  a.addEventListener("click", function(event) {
+	    event.preventDefault();
+	    event.stopPropagation();
+	  });
+	});
+	
+	const navTwo = document.querySelector(".main-navigation");
+	
+	navTwo.addEventListener("click", function(event) {
+	  alert("main-nav");
+	});
